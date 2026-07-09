7de7cd5a7f6a9db2ff766ecbacd5ba86724ee3b6
jokoson <a@a.aa>
2026-07-09 20:09:10 +0200

    remove comments from quota API route

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,9 +4,7 @@ import {requireAuth} from "../middleware/auth.js";
 
 const router = express.Router();
 
-// Route: http://localhost:3001/api/quota
 router.get("/quota", requireAuth, (req, res) => {
-    // Liefert das im RAM zusammengeführte, vollständige Objekt zurück
     res.json(getQuota());
 });
 
